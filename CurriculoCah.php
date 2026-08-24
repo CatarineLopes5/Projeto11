d2e5ac2f12e36a3a26b10ccd8c093b6589d46abf
CatarineLopes5 <catarine.sales@aluno.cps.sp.gov.br>
2026-08-24 18:08:01 -0300

    Commit pagina principal e curriculo

---
+++ CurriculoCah.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currículo Catarine</title>
+</head>
+<body>
+    <div class="page">
+    <div class="cabeçalho">
+        <p> cabeçalho </p>
+
+    </div>
+
+    <hr>
+
+    <div class="conteiner1">
+        <p> Objetivo </p>
+
+        <p>Resumo das qualificações </p>
+
+        <p>Formação academica</p>
+
+    </div>
+
+    <hr>
+
+    <div class="container2">
+        <p>ExPERIÊNCIA PROFISSIONALL</p>
+
+    </div>
+
+    <hr>
+
+    <div class="Conteiner3">
+        <p>Cursos</p>
+
+        <p>idiomas</p>
+
+        <p>Informações Adicionais</p>
+
+
+    </div>
+
+    <?php
+
+    echo "Pru";
+
+    ?>
+
+</div>
+</body>
+</html>
