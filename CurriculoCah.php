f7ae1075dd88aadc53c6e3e220afe3f7406aea8a
CatarineLopes5 <catarine.sales@aluno.cps.sp.gov.br>
2026-08-25 21:25:39 -0300

    Update CurriculoCah.php

--- CurriculoCah.php
+++ CurriculoCah.php
@@ -8,34 +8,70 @@
 <body>
     <div class="page">
     <div class="cabeçalho">
-        <p> cabeçalho </p>
-
+        <h1> CATARINE LOPES DA SILVA SALES </h1>
+        <p> 23 anos | Centro - Diadema | São Paulo | Cel: (11) 96916-2280</p>
+        <p>Email: catarinesales2013@gmail.com</p>
+        <p>GitHub/Portifólio: <a href:"https://github.com/CatarineSales"</p>
     </div>
 
     <hr>
 
     <div class="conteiner1">
-        <p> Objetivo </p>
+        <h2> Objetivo </h2>
+        <p>Estagiária na áreas de Desenvolvimento de Sistemas</p>
 
-        <p>Resumo das qualificações </p>
+        <br>
 
-        <p>Formação academica</p>
+        <h2>Resumo das qualificações </h2>
+        <p>Desenvolvedora Front End com conhecimento principal em linguagens HTML, CSS, JavaScript,Kotlin e C, alem de conhecimentos em Design Digital. Criativa, comunicativa e possui aptidão como autodidata, buscando contantemente seu desenvolvimento profissional.</p>
+
+        <br>
+
+        <h2>Formação academica</h2>
+        <p><strong>Ensino Superior</strong> 2026 | Concluído</p>
+        <p>Desenvolvimento de Software | FATEC Diadema</p>
+
+        <p><strong>Curso Técnico</strong> 2026 | Em Andamento</p>
+        <p>Desenvolvimento de Sistemas | ETEC JK Diadema</p>
 
     </div>
 
     <hr>
 
     <div class="container2">
-        <p>ExPERIÊNCIA PROFISSIONALL</p>
+        <h2>EXPERIÊNCIA PROFISSIONAL</h2>
+        <p><strong>Freelancer Desenvolvimento FrontEnd - SP</strong></p>
+        <p>Em 2025 participaou da reformulação de uma página de uma pagina de cardápio online de um ambiente de festas e espetarias de forma voluntária.</p>
+
+        <p><strong>Freelancer Desenvolvimento FrontEnd Mobile- SP</strong></p>
+        <p>Em Fevereiro de 2026 foi convidada para participar do desenvovlvimento FrontEnd de um aplicativo chamado Árbitro Varzea, projeto em parceria com alunas da FATEC de esportes.</p>
+        
 
     </div>
 
     <hr>
 
     <div class="Conteiner3">
-        <p>Cursos</p>
+        <h2>Cursos</h2>
+        <p>HTML, CSS and JavaScript Essentials (2025) - Cursando | Cisco | Online</p>
+        <p>Pyston Essentials (2023) - Concluído | Cisco | Online</p>
 
-        <p>idiomas</p>
+        <br>
+
+        <h2>idiomas</h2>
+        <p> Inglês - Intermediário
+        <p> Espanhol - Intermediário
+
+        <br>
+
+        <h2>INFORMAÇÕES ADICIONAIS</h2>
+        <p>Antes de entrar na fatec, trabalhava como freelancer com fotografia e com audiovisual como Assitente de Direção.</p>
+        <p>Em 2024 participou da Iniciação Cientifica da FATEC, com foco na pesquisa com tema "Arte e Tecnologia: as contribuições e desafios nos processsos criativos dos artistas".</p>
+        <p>No segundo semestre do mesmo ano apresentou sua pesquisa no Congresso Nacional de Iniciação.</p>
+
+        
+        
+        
 
         <p>Informações Adicionais</p>
 
