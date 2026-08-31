0678667d8a8287983e72c90395273439af0d74dd
cris4226 <cristiane.carvalho01@aluno.cps.sp.gov.br>
2026-08-30 23:12:09 -0300

    Atualizar o CurriculoCris.php

--- CurriculoCris.php
+++ CurriculoCris.php
@@ -2,12 +2,10 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CurriculoCris</title>
+    <title>Curriculo-Cristiane Carvalho</title>
 </head>
 <body>
-    <?php
-    echo "projeto"
+    </head
 
     ?>
     
