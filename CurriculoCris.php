72badf9b70c89620dd4ffb2a69fb975068544c6b
cris4226 <cristiane.carvalho01@aluno.cps.sp.gov.br>
2026-08-31 17:30:58 -0300

    Atualizar o CurriculoCris.php

--- CurriculoCris.php
+++ CurriculoCris.php
@@ -6,8 +6,73 @@
 </head>
 <body>
     </head
+      <h1>Cristiane Carvalho</h1>
+      <p><strong>Objetivo:</strong>Estágio Desenvolvimento de Sistema e Auxiliar Administrativo</p>
+      <p>
+        Telefone:(11) 9 6933-1993<br>
+        Localização: São Paulo - SP <br>
+        E-mail: cristianepeixoto@outlook.com.br </p>
+      </p>
+</header>
 
-    ?>
+<hr>
+  <section>
+       <h2>Sobre Mim</h2>
+      <p>
+    Sou uma pessoa dedicada,responsável, comunicativa e sempre disposta a aprender novas habilidades.
+    Tenho interesse em tecnologia, informática e desenvolvimento de web,além de gostar de trabalhar em equipe e aplicar meus conhecimentos, alcançar bons resultados para crescer profissionalmente.
+      <p>
+    <section>
+     <hr>
+    </section>
+    <h2>Experiência Profissional </h2>
+
+    <h3>Galeteria Frangolito </h3>
+    <p><strong>Cargo:</strong>Gerente</p>
+    <p><strong>Perìodo:<strong>06/2019</p>
+     
+    <h3>Sãlao de Beleza ANALU</h3>
+    <p><strong>Cargo:</strong>Cabelereira</p>
+    <p><strong>Período:</strong>04/2008 a 06/2015</p>
+
+  </section>
+
+   <hr>
+  <section>
+   <h2>Educação  e Cursos</h2>
+   <h3>ETEC Juscelino Kubitschek de   Oliveira</h3>
+   <p>Técnico em Desenvolvimento de Sistemas </p>
+    <p><strong>Conclusão:</strong>Julho/2027</p>
+    <h3>Associação Comunidade Despertar</h3>
+    <p>Assistente Administrativo (Carga Horária: 330 Horas)</p>
+    <p><strong>Conclusão:</strong>Junho/2025</p>
+
+    <h3>Fundação  Florestan Fernandes </h3>
+    <p>Departamento Pessoal (Carga Horária:400 Horas)<p>
+    <p>strong>Conclusão:</strong>Maio/202=</p>
+    
+    <h3>E.E.Prof°Nicéia Albarello Ferrari</h3>
+    <p>Ensino Médio - Concluído </p>
+    <p><strong>Conclusão:</strong>Dezembro/2005</p>
+    </section>
+    <hr>
+
+    <section>
+     <h2>Qualificação Profissional </h2>
+     <ul>
+        <li>Pacote Office Básico </li>
+        <li>Rotinas Administrativas </li>
+        <li>linux e Sistema Embarcados</li>
+        <li>MySL e HTML </li>
+        <li>Linguagem C e C++</li>
+        <li>Trabalho em equipe, Pontualidade  e Proatividade</li>
+        </ul>
+        
+     </section>
+ 
+    </body>
+
+    </html>
     
 </body>
 </html>
